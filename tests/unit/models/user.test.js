8ca5799da0a4988b0e8387f9f519f3190e02e25d
Ankit-Dwij <ankitdwiz010@gmail.com>
2022-10-15 23:41:44 +0530

    added Dockerfile

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -1,4 +1,4 @@
-const { User } = require("../../../../models/userSchema");
+const { User } = require("../../../models/userSchema");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
